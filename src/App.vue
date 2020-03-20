e222cccb4af872d7849f83259a76519879f0e1a7
Vincent Guegan <gueganvincent@gmail.com>
2020-03-20 10:47:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+
+    <TopToolbar></TopToolbar>
+
+    <BottomNav></BottomNav>
+
+  </v-app>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import TopToolbar from './components/TopToolbar.vue';
+  import BottomNav from './components/BottomNav.vue';
+  @Component({
+    components: {
+      TopToolbar,
+      BottomNav
+    }
+  })
+  export default class extends Vue {
+  }
+</script>
